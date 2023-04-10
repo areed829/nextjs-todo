09831c54dd70b3b9a1375292fbd71bf38ed0aa07
Alex Reed <areed829@gmail.com>
2023-04-09 20:30:54 -0400

    style: :lipstick: Add style to the form

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,26 @@
 export default function Home() {
   return (
-    <div className='flex flex-col items-center'>
-      <div className=''>
-        <label htmlFor='taskInput'>Task</label>
-        <input id='taskInput' type='text' />
+    <>
+      <div className='min-h-screen flex items-center justify-center'>
+        <form className='bg-white p-6 rounded shadow-md w-full max-w-md'>
+          <div className='mb-4'>
+            <label htmlFor='todo' className='block mb-2 text-sm font-bold'>
+              Todo:
+            </label>
+            <input
+              type='text'
+              id='todo'
+              className='w-full p-2 border border-gray-300 rounded'
+            />
+          </div>
+          <button
+            type='submit'
+            className='w-full bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded'
+          >
+            Submit
+          </button>
+        </form>
       </div>
-    </div>
+    </>
   );
 }
